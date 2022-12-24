aa5e56d810a3636e4a96840c4f1ff3cad254d522
rohan <rohannak2057@gmail.com>
2022-12-24 15:06:36 +0530

    adds client side logics

---
+++ client/script.js
@@ -0,0 +1,125 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+    element.textContent = '';
+    loadInterval = setInterval(() => {
+
+        element.textContent += '.';
+
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+
+    }, 300);
+}
+
+function typeText(element, text) {
+    let index = 0;
+    let typeInterval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHtml += text.charAt(index);
+            index++;
+        }
+        else {
+            clearInterval(typeInterval);
+        }
+
+    }, 20)
+}
+
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+
+}
+
+function chatPanel(isAi, value, uniqueId) {
+    return (
+        `
+    <div class="wrapper" ${isAi && 'ai'}>
+      <div class="chat">
+         <div class="profile">
+          <img 
+           src="${isAi ? bot : user}"
+           alt="${isAi ? "bot" : "user"}"
+          />      
+         </div>
+         <div class="message" id=${uniqueId}>${value}</div>
+      </div> 
+     </div>
+    `
+    )
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+
+    //user's chat panel
+    chatContainer.innerHTML += chatPanel(false, data.get('prompt'));
+    form.reset();
+
+    //bot's chat panel
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatPanel(true, " ", uniqueId);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    const messageDiv = document.getElementById(uniqueId);
+
+    loader(messageDiv);
+
+    // fetch data from server --> bot's response
+
+    const response = await fetch('http://localhost:8000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    });
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = '';
+
+
+    const parseData = response.bot.trim;
+
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode == 13)
+        handleSubmit(e);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
